03ba326f6e3d2e6eb49dceeec5a69ae8a7e07f6f
Fokusmod <Info@mail.ru>
2022-06-17 17:17:06 +0300

    test_2.1

--- src/main/java/Test2CommitClass.java
+++ src/main/java/Test2CommitClass.java
@@ -2,5 +2,9 @@ public class Test2CommitClass {
 
     public static void main(String[] args) {
         System.out.println("Test 2");
+        System.out.println("Test 2");
+        System.out.println("Test 2");
+        System.out.println("Test 2");
+        System.out.println("Test 2");
     }
 }
